de976aa9c2432906d9d0e09a0d0234bb11267a70
Student <Student@COMP07>
2017-11-03 12:11:38 +0200

    ---

--- AutoTests/MenuBar.cs
+++ AutoTests/MenuBar.cs
@@ -11,9 +11,17 @@ namespace AutoTests
     {
         static Window window = null;
         static Application application = null;
-
+        /*
         [ClassInitialize]
         public static void ClassInit(TestContext t)
+        {
+            application = Application.Launch(@"C:\Users\Student\Desktop\01.11\CodePrototype\CodePrototype\bin\Debug\CodePrototype");
+            window = application.GetWindow("FigurePainter", InitializeOption.NoCache);
+            POM.Window = window;
+        }
+        */
+        [TestInitialize]
+        public void TestInit()
         {
             application = Application.Launch(@"C:\Users\Student\Desktop\01.11\CodePrototype\CodePrototype\bin\Debug\CodePrototype");
             window = application.GetWindow("FigurePainter", InitializeOption.NoCache);
